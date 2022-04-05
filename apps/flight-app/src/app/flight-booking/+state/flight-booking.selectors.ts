68200e666a1bf0eca8fa6bbb4b0c675208159f19
mikezks <23002661+mikezks@users.noreply.github.com>
2022-04-05 15:20:33 +0200

    feat: redux selectors

--- apps/flight-app/src/app/flight-booking/+state/flight-booking.selectors.ts
+++ apps/flight-app/src/app/flight-booking/+state/flight-booking.selectors.ts
@@ -4,3 +4,42 @@ import * as fromFlightBooking from './flight-booking.reducer';
 export const selectFlightBookingState = createFeatureSelector<fromFlightBooking.State>(
   fromFlightBooking.flightBookingFeatureKey
 );
+
+export const selectFlights = createSelector(
+  // Selectors
+  selectFlightBookingState,
+  // Projector
+  state => state.flights
+);
+
+export const selectPassengers = createSelector(
+  selectFlightBookingState,
+  (state) => state.passenger
+);
+
+export const selectBookings = createSelector(
+  selectFlightBookingState,
+  (state) => state.bookings
+);
+
+export const selectUser = createSelector(
+  selectFlightBookingState,
+  (state) => state.user
+);
+
+export const selectActiveUserFlights = createSelector(
+  // Selectors
+  selectFlights,
+  selectBookings,
+  selectUser,
+  // Projector
+  (flights, booking, user) => {
+    const activeUserId = user.passengerId;
+    const activeUserFlightIds = booking
+      .filter(b => b.passengerId === activeUserId)
+      .map(b => b.flightId);
+    const activeUserFlights = flights
+      .filter(f => activeUserFlightIds.includes(f.id));
+    return activeUserFlights;
+  }
+)
